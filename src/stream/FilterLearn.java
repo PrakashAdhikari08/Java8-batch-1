5e0c56a6c2a9344704568bad3a4a2af4311b2a9c
Prakash <prakashadhikarijhapali@gmail.com>
2020-10-14 22:10:36 +1100

    Filter in stream done. The use of condition. Filter takes the Predicates For each takes Consumer

--- src/stream/FilterLearn.java
+++ src/stream/FilterLearn.java
@@ -1,4 +1,22 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FilterLearn {
+
+    public static void main(String[] args) {
+
+        List<String> listOfName = Arrays.asList("Hari","sita","raju", "ravi");
+
+
+        //write your logic for filtering inside the filter
+        listOfName.stream().filter(name -> name.equals("raju")).forEach(name -> System.out.println(name));
+
+        listOfName.stream().filter(name -> !name.equals("raju")).forEach(name -> System.out.println(name));
+
+        listOfName.stream().filter(name -> listOfName.size()>2).forEach(name -> System.out.println(name));
+
+
+    }
 }
